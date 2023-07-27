2d4c0b6809ec07263c9fb3d3ecb6b7fc51646d3b
NubTheFatMan <that1nub@gmail.com>
2023-07-26 17:37:49 -0700

    more details in stealsticker command

--- plugins/commands/stealsticker.js
+++ plugins/commands/stealsticker.js
@@ -5,11 +5,21 @@ exports.calls = ["stealsticker", "ss", "sticker", "stickerlink"];
 exports.callback = (message, args) => {
     message.fetchReference().then(messageWithSticker => {
         if (messageWithSticker.stickers.size > 0) {
-            message.reply(messageWithSticker.stickers.first().url);
+            let sticker = messageWithSticker.stickers.first();
+
+            let lines = [];
+            lines.push(`Name: **${sticker.name}**`);
+            if (sticker.description) lines.push(`Description: ${sticker.description}`);
+
+            let created = Math.round(sticker.createdTimestamp / 1000);
+            lines.push(`Created: <t:${created}:f> (<t:${created}:R>)`);
+
+            lines.push(sticker.url);
+            message.reply(lines.join('\n'));
         } else {
             message.reply("No sticker attached to this message.");
         }
     }).catch(err => {
-        message.reply('You must reply to a message.');
+        message.reply('You must reply to a message with a sticker.');
     });
 }
